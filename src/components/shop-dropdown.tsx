92c7a2eb9911a08e97a5abc8fb81b85f5676e112
Karim <devkarim@hotmail.com>
2023-07-19 21:29:06 +0300

    Add zustand and 'create new shop' option in shops dropdown

--- src/components/shop-dropdown.tsx
+++ src/components/shop-dropdown.tsx
@@ -1,5 +1,6 @@
-import { FaArrowDown } from 'react-icons/fa';
+import Link from 'next/link';
 import { MdStore } from 'react-icons/md';
+import { FaArrowDown, FaPlusCircle } from 'react-icons/fa';
 
 import { Shop } from '@prisma/client';
 import { cls } from '@/lib/utils';
@@ -9,46 +10,33 @@ interface ShopDropdownProps {
 }
 
 const ShopDropdown: React.FC<ShopDropdownProps> = ({ shops }) => {
-  const disabled = shops.length == 0;
-
   return (
-    <div className="dropdown">
+    <div className="dropdown cursor-pointer">
       <div
         tabIndex={0}
         className="flex items-center border-2 space-x-8 rounded-lg p-2 text-sm"
       >
         <div className="flex items-center space-x-2 text-sm">
-          <MdStore
-            className={cls('text-xl opacity-100', {
-              'opacity-60': disabled,
-            })}
-          />
-          <p
-            className={cls('font-medium opacity-100', {
-              'opacity-60': disabled,
-            })}
-          >
-            Choose your shop
-          </p>
+          <MdStore className={cls('text-xl opacity-100')} />
+          <p className={cls('font-medium opacity-100')}>Choose your shop</p>
         </div>
-        <FaArrowDown
-          className={cls('opacity-100', {
-            'opacity-60': disabled,
-          })}
-        />
+        <FaArrowDown className={cls('opacity-100')} />
       </div>
-      {shops.length != 0 && (
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          {shops.map((s) => (
-            <li key={s.id}>
-              <a>{s.name}</a>
-            </li>
-          ))}
-        </ul>
-      )}
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-lg w-52"
+      >
+        {shops.map((s) => (
+          <li key={s.id}>
+            <a>{s.name}</a>
+          </li>
+        ))}
+        <li className={cls({ 'border-t': shops.length !== 0 })}>
+          <Link href="/">
+            <FaPlusCircle /> Create a new shop
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
